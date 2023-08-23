6fa42572c673c5cf99af3cc2b75f7de56cda556d
AmirSmith413 <amirsmith166@gmail.com>
2023-08-22 21:32:27 -0700

    made dropdown for the questions,need to figure out api next

--- FlashCard/src/components/SortCategory.tsx
+++ FlashCard/src/components/SortCategory.tsx
@@ -1,15 +1,35 @@
-import { Button, Menu, MenuButton, MenuList } from '@chakra-ui/react'
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import {BsChevronDown} from 'react-icons/bs'
 
+interface Props{
+  onSelectSortCategory:(sortCategory: string) => void
+  sortCategory:string
+}
 
-const SortCategory = () => {
+const SortCategory = ({onSelectSortCategory,sortCategory}:Props) => {
+  const sortCategories = [
+    { value: "", label: "Video Games" },
+    { value: "-", label: "Books" },
+    { value: "name", label: "General Knowledge" },
+    { value: "-released", label: "History" },
+    { value: "-rating", label: "Television" },
+  ]
+  const currentSortCategory = sortCategories.find(order => order.value === sortCategory)
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown/>}>
-        
+        Category: {currentSortCategory?.label || 'Video Games'}
       </MenuButton>
       <MenuList>
-        
+        {sortCategories.map((order) => (
+          <MenuItem
+          onClick={() => onSelectSortCategory(order.value)}
+          key={order.value}
+          value={order.value}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   )
--- FlashCard/src/hooks/useData.ts
+++ FlashCard/src/hooks/useData.ts
@@ -1,7 +1,11 @@
-import React from 'react'
+import apiClient from "../services/api-client"
+import {useEffect} from "react"
+
 
 const useData = () => {
-  
+  useEffect(() => {
+    const controller = new AbortController()
+  })
 }
 
 export default useData
