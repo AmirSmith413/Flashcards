d71207bb504ffd7b2deafe94422f22f9686bedef
AmirSmith413 <amirsmith166@gmail.com>
2023-08-31 22:22:12 -0700

    moved dropdown to new page

---
+++ FlashCard/src/components/Categories.tsx
@@ -0,0 +1,32 @@
+import { FormControl, FormLabel, Select } from '@chakra-ui/react';
+
+interface Props {
+    selectedCategory: string;
+    onCategoryChange: (category: string) => void;
+}
+
+const Categories = ({ selectedCategory, onCategoryChange }:Props) => {
+  const categories = [
+    { id: 10, name: 'Entertainment: Books' }, 
+    { id: 11, name: 'Entertainment: Film' },
+    { id: 15, name: 'Entertainment: Video Games' },
+    { id: 16, name: 'Entertainment: Board Games' },
+    { id: 12, name: 'Entertainment: Music' },
+
+  ];
+
+  return (
+    <FormControl mr={'-200px'} mb={'200px'} width={'200px'}>
+      <FormLabel>Select Category</FormLabel>
+      <Select value={selectedCategory} onChange={(e) => onCategoryChange(e.target.value)}>
+        {categories.map((category) => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default Categories;
--- FlashCard/src/components/Flashcard.tsx
+++ FlashCard/src/components/Flashcard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, FormControl, FormLabel, Grid, GridItem, Select, Text } from "@chakra-ui/react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Categories from "./Categories";
 
 
 interface CardProps {
@@ -10,19 +11,10 @@ interface CardProps {
   correct_answer: string;
   question: string;
   name: string;
-  selectedCategory: number;
-  onCategoryChange: (category: number) => void;
+  
 }
 
-const Flashcard = ({selectedCategory,onCategoryChange}:CardProps) => {
-  const sortCategory = [
-    {id:15,name:'Video Games'},
-    {id:27,name:'Animals'},
-    {id:14,name:'Televison'},
-    {id:16,name:'Board Games'},
-    {id:12,name:'Music'},
-
-  ]
+const Flashcard = () => {
   const [flip, setFlip] = useState(false)
   const [data, setData] = useState<CardProps[]>([]);
   const handleFlip = () => {
@@ -47,16 +39,9 @@ const Flashcard = ({selectedCategory,onCategoryChange}:CardProps) => {
   }
   return (
     <>
-     <FormControl mb={'200px'} width={'200px'}>
-      <FormLabel>Select Category</FormLabel>
-      <Select value={selectedCategory} onChange={(e) => onCategoryChange(Number(e.target.value))}>
-        {sortCategory.map((category) => (
-          <option key={category.id} value={category.id}>
-            {category.name}
-          </option>
-        ))}
-      </Select>
-    </FormControl>
+   <Categories selectedCategory={""} onCategoryChange={function (category: string): void {
+        throw new Error("Function not implemented.");
+      } }/>
       <Grid templateColumns='repeat(auto-fit,minmax(200px,1fr))'
               gap={5}>
         {data.map(bleh => (
@@ -71,27 +56,7 @@ const Flashcard = ({selectedCategory,onCategoryChange}:CardProps) => {
 
 
 
-        {/* <CardBody>
-          {data.map((helpme) => (
-            <p key={helpme.id}>{helpme.difficulty}</p>
-          ))}
-          {data.map((helpme) => (
-            <p key={helpme.id}>{helpme.question}</p>
-          ))}
-          {data.map((helpme) => (
-            <p key={helpme.id} >{helpme.correct_answer}</p>
-          ))}
-          {data.map((helpme) => (
-            <p key={helpme.id} >{helpme.correct_answer}</p>
-          ))}
-          {data.map((helpme) => (
-            <p key={helpme.id} >{helpme.correct_answer}</p>
-          ))}
-          {data.map((helpme) => (
-            <p key={helpme.id} >{helpme.correct_answer}</p>
-          ))}
-          
-        </CardBody> */}
+     
       </Grid>
     
     </>
